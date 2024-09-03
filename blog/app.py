0ac0a00158d64832d7b2e507165bc69597c8d5c9
oleg <oleg@rusefs.com>
2024-09-03 22:17:54 +0300

    not complited task

--- blog/app.py
+++ blog/app.py
@@ -1,12 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 from blog.user.views import user
 from blog.report.views import report
 
 
+db = SQLAlchemy()
+
 def create_app() -> Flask:
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_pyfile('config.py')
+    print("DEBUG:", app.config.get("DEBUG"))
+    print("DATABASE_URI:", app.config.get("SQLALCHEMY_DATABASE_URI"))
+    print("Secret Key:", app.config.get("SECRET_KEY"))
+    db.init_app(app)
+
     register_blueprints(app)
-    return app 
+    return app
 
 
 def register_blueprints(app: Flask) -> None:
--- blog/report/views.py
+++ blog/report/views.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template
 report = Blueprint(
     'report',
     __name__,
-    url_prefix='/report',
+    url_prefix='/reports',
     static_folder='../static',
     template_folder='../templates',
 )
